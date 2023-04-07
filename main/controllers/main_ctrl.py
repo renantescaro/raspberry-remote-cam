ec01361707c2938fc92aac6711a43680eba7a11a
renantescaro <renantescaro@yahoo.com.br>
2023-04-07 20:01:49 -0300

    fix cors

--- main/controllers/main_ctrl.py
+++ main/controllers/main_ctrl.py
@@ -1,5 +1,7 @@
 from flask import render_template, Blueprint
 
+from main.services.setting import Setting
+
 bp = Blueprint(
     'main',
     __name__,
@@ -11,5 +13,6 @@ class MainCtrl:
     def main_route():
         return render_template(
             template_name_or_list='cam.html',
-            title='Controle da camera'
+            title='Controle da camera',
+            server_ip=Setting.get('EXTERNAL_IP')
         )
