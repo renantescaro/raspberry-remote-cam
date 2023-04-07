30f709182e86e09da9bfb95b77d71bdea4b95e38
renantescaro <renantescaro@yahoo.com.br>
2023-04-07 18:34:28 -0300

    feat: add cors

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
 from main.controllers import blueprints_ctrl
+from main.services.setting import Setting
 
 app = Flask(
     __name__,
     template_folder='main/templates'
+)
+CORS(
+    app,
+    resources={
+        r'/cam/*': {'origins': Setting.get('EXTERNAL_IP')}
+    }
 )
 
 for bp in blueprints_ctrl:
